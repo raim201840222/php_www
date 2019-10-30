4b21b7861e9263fbc56f46850d04521676a65fae
gwangmin <inobicha@gmail.com>
2019-10-30 15:01:25 +0900

    컨트롤러 분리

---
+++ App/Controller/TableInfo.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controller;
+    class TableInfo{
+        // 생성자
+        public function __construct(){
+            echo __CLASS__;
+        }
+        public function main(){
+            echo "메인 호출입니다.";
+        }
+    }
--- public/index.php
+++ public/index.php
@@ -1,36 +1,21 @@
 <?php
 $config = include "../dbconf.php";
-//echo "대림대학교";
-//print_r($config);
-
 require "../Loading.php";
-/*
-require "../Module/Database/database.php"; //1개
-require "../Module/Database/table.php"; //2개
-*/
-
-$db = new \Module\Database\Database($config);
-$html = new \Module\Html\HtmlTable;
-
-$query = "SHOW TABLES";
-$result = $db->queryExecute($query);
-
-$count = mysqli_num_rows($result);
-$content = ""; //초기화
-$rows = []; //배열 초기화
-for($i=0;$i<$count;$i++){
-    $row = mysqli_fetch_object($result);
-    $rows []= $row; //배열 추가
-    /*
-    $content .="<tr>";
-    $content .="<td>$i</td>";
-    $content .= "<td>".$row->Tables_in_php."</td>";
-    $content .="</tr>";
-    */
+$uri = $_SERVER['REQUEST_URI'];
+$uris = explode("/",$uri); // 파란책
+print_r($uris);
+$db = new \Module\Database\Database( $config );
+if(isset($uris[1]) && $uris[1]) {
+    // 컨트롤러 실행...
+    echo $uris[1]."컨트롤러 실행...";
+    $controllerName = "\App\Controller\\" . ucfirst($uris[1]);
+    $tables = new $controllerName ($db);
+    $tables->main();
+} else {
+    // 처음 페이지 에요.
+    // echo "처음 페이지 에요.";
+    $body = file_get_contents("../Resource/index.html");
+    echo $body;
 }
-$content = $html->table($rows);
-
-$body = file_get_contents("../Resource/table.html");
-$body = str_replace("{{content}}",$content, $body); // 데이터 치환
-echo $body;
-
+// $desc = new \App\Controller\TableInfo;
+// $desc->main();
