915af63c4eba2ddc7417b3a619fb6e71176e629e
gwangmin <inobicha@gmail.com>
2019-10-30 15:29:02 +0900

    sql desc

--- App/Controller/TableInfo.php
+++ App/Controller/TableInfo.php
@@ -2,10 +2,28 @@
 namespace App\Controller;
     class TableInfo{
         // 생성자
-        public function __construct(){
-            echo __CLASS__;
+        private $db;
+        public function __construct($db){
+            // echo __CLASS__;
+            $this->db = $db;
         }
         public function main(){
-            echo "메인 호출입니다.";
+            //echo "메인 호출입니다.";
+            $query = "Desc members";
+            $result = $this->db->queryExecute($query);
+
+            $count = mysqli_num_rows($result);
+            $content = ""; //초기화
+            $rows = []; //배열 초기화
+            for($i=0;$i<$count;$i++){
+                $row = mysqli_fetch_object($result);
+                // $rows []= $row; //배열 추가
+                print_r($row);
+                foreach($row as $r){
+                    echo $r." ";
+                }
+                echo "<br>";
+          
         }
     }
+}
